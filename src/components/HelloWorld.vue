12695bb727f44295a86412d46442308a5f638730
tangcheng888 <1181762742@qq.com>
2021-03-31 17:30:59 +0800

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <router-link to="./a">去a页</router-link>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
---
+++ src/components/a.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      a
+  </div>
+</template>
+<script>
+export default {
+  props: [],
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {},
+};
+</script>
+<style scoped>
+</style>
